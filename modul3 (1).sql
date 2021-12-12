9b445aafec5aa951b7131db658f4e2a25652a04c
dea <dhesihotang14@gmail.com>
2021-12-12 20:55:42 +0700

    Create modul3 (1).sql

---
+++ modul3 (1).sql
@@ -0,0 +1,66 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 21 Nov 2021 pada 10.06
+-- Versi server: 10.4.8-MariaDB
+-- Versi PHP: 7.3.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `modul3`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `buku_table`
+--
+
+CREATE TABLE `buku_table` (
+  `id_buku` int(100) NOT NULL,
+  `judul_buku` varchar(255) NOT NULL,
+  `penulis_buku` varchar(255) NOT NULL,
+  `tahun_terbit` int(100) NOT NULL,
+  `deskripsi` text NOT NULL,
+  `gambar` varchar(255) NOT NULL,
+  `tag` varchar(255) NOT NULL,
+  `bahasa` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+-- Kesalahan membaca data untuk tabel modul3.buku_table: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `modul3`.`buku_table`' at line 1
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `buku_table`
+--
+ALTER TABLE `buku_table`
+  ADD PRIMARY KEY (`id_buku`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `buku_table`
+--
+ALTER TABLE `buku_table`
+  MODIFY `id_buku` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
